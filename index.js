f1646994dcbc63a5ec942a5301eb11df36b52b48
unknown <shweboy@gmail.com>
2018-11-11 15:56:02 +0700

    added controller to index.js to remove 404 error while setting up webhook on fb app.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const verificationController = require('./controllers/verification');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/',verificationController);
 app.listen(3000, () => console.log('Webhook server is listening, port 3000'));
